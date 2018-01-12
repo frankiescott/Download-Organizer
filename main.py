031d9b02f402e56de1914153b263219f791554cb
Frankie <email@frankiescott.net>
2018-01-12 18:12:49 -0500

    added an undo function

--- main.py
+++ main.py
@@ -7,17 +7,35 @@ from tkinter import messagebox
 root = Tk()
 root.withdraw()
 directory = filedialog.askdirectory(initialdir="C:\\")
-result = messagebox.askquestion("File Organizer", "The directory " + directory + " will be organized. Continue?", icon="warning")
+if directory == "":
+    quit()
 
-if result == "yes":
-    for filename in os.listdir(directory):
-        extension = os.path.splitext(filename)[1][1:]
+result = messagebox.askquestion("File Organizer", directory + " will be organized. Continue?", icon="warning")
 
-        #ignore folders
-        if os.path.isdir(directory + "/" + filename):
-            continue
-        #create a folder for each extension
-        if not os.path.exists(directory + extension):
-            os.makedirs(directory + extension)
+if result == "no":
+    quit()
 
-        shutil.move(directory + "/" + filename, directory + "/" + extension + "/" + filename)
+filelist = []
+count = 0
+for filename in os.listdir(directory):
+    extension = os.path.splitext(filename)[1][1:]
+
+    #ignore folders
+    if os.path.isdir(directory + "/" + filename):
+        continue
+    #create a folder for each extension
+    if not os.path.exists(directory + extension):
+        os.makedirs(directory + extension)
+
+    filelist.append([filename, directory + "/" + extension + "/" + filename])
+    shutil.move(directory + "/" + filename, directory + "/" + extension + "/" + filename)
+    count += 1
+
+if count == 0:
+    messagebox.showinfo("File Organizer", "No files were found in " + directory)
+    quit()
+
+okcancel = messagebox.askokcancel("File Organizer", str(count) + " files were moved. Press Cancel to undo these changes or OK to continue.")
+if okcancel == False:
+    for f in filelist:
+        shutil.move(f[1], directory + "/" + f[0])
